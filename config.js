086dcc9af0f8ce399f47e8c429fa35f0d5385194
bibliotecabruta <bibliotecabruta@gmail.com>
2026-09-14 00:20:46 -0300

    Ativa Google Analytics 4

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.BB_CONFIG = {
   supabaseUrl: "https://mjlujvpvpxelpcbxgleo.supabase.co",
   supabasePublishableKey: "sb_publishable_ld05q10JQ2VePslq49x95w_CFHlfxuE",
-  ga4MeasurementId: ""
+  ga4MeasurementId: "G-Y8C89KWFHN"
 };
