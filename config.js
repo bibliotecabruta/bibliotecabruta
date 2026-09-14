f57687934476d148c4a53a7508b72506b68f01c1
bibliotecabruta <bibliotecabruta@gmail.com>
2026-09-13 23:51:09 -0300

    Prepara configuração do GA4 sem ativar rastreamento

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 window.BB_CONFIG = {
   supabaseUrl: "https://mjlujvpvpxelpcbxgleo.supabase.co",
-  supabasePublishableKey: "sb_publishable_ld05q10JQ2VePslq49x95w_CFHlfxuE"
+  supabasePublishableKey: "sb_publishable_ld05q10JQ2VePslq49x95w_CFHlfxuE",
+  ga4MeasurementId: ""
 };
