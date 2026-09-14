8da81943c366e65d1e28472ed7196bc7a88580dd
bibliotecabruta <bibliotecabruta@gmail.com>
2026-09-14 18:12:45 -0300

    Preserva sitemaps SEO de livros e séries

--- scripts/generate-sitemaps.mjs
+++ scripts/generate-sitemaps.mjs
@@ -17,9 +17,7 @@ for(let start=0;;start+=1000){
 }
 
 const specs={
- book:['sitemap-books.xml',r=>'livro.html?id='+encodeURIComponent(r.id)],
  author:['sitemap-authors.xml',r=>'autor.html?id='+encodeURIComponent(r.id)],
- series:['sitemap-series.xml',r=>'serie.html?id='+encodeURIComponent(r.id)],
  category:['sitemap-categories.xml',r=>'categoria.html?id='+encodeURIComponent(r.id)],
  theme:['sitemap-themes.xml',r=>'tema.html?id='+encodeURIComponent(r.id)],
  news:['sitemap-news.xml',r=>r.slug?'noticia.html?slug='+encodeURIComponent(r.slug):'noticia.html?id='+encodeURIComponent(r.id)]
